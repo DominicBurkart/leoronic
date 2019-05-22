e4f0483a0aa4ed295b5723c05afe8a566a17b1a9
Dominic Burkart <1dominicb@gmail.com>
2019-05-22 18:30:38 -0400

    add docker_wrapper_template.py

---
+++ wrappers/python3/docker_wrapper_template.py
@@ -0,0 +1,13 @@
+import dill
+import base64
+import pipes
+
+t = pipes.Template()
+t.append("tr a-z A-Z", "--")
+result_pipe = t.open("result", "w")
+f = dill.loads(base64.b64decode({}))  # type: ignore
+try:
+    result_pipe.write("r" + str(base64.b64encode(dill.dumps(f()))))
+except Exception as e:
+    result_pipe.write("e" + str(base64.b64encode(dill.dumps(e))))
+result_pipe.close()
