daf025e6e5c492f3dc3ba4f879c23cc4a89b906b
Dominic Burkart <1dominicb@gmail.com>
2019-05-22 22:24:16 -0400

    fix typo

--- wrappers/python3/docker_wrapper_template.py
+++ wrappers/python3/docker_wrapper_template.py
@@ -5,7 +5,7 @@ import pipes
 t = pipes.Template()
 t.append("tr a-z A-Z", "--")
 result_pipe = t.open("result", "w")
-f = dill.loads(base64.b64decode({}))  # type: ignore
+f = dill.loads(base64.b64decode("{}"))
 try:
     result_pipe.write("r" + str(base64.b64encode(dill.dumps(f()))))
 except Exception as e:
